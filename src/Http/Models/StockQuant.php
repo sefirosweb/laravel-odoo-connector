439915f38daf658e1393e3ed1de8382d1f839ec1
sefirosweb <sefirosweb@gmail.com>
2025-09-15 11:38:07 +0200

    Added product in stock quant

--- src/Http/Models/StockQuant.php
+++ src/Http/Models/StockQuant.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Sefirosweb\LaravelOdooConnector\Http\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class StockQuant extends OdooModel
 {
     protected $table = 'stock.quant';
+
+    public function product_product(): BelongsTo
+    {
+        return $this->belongsTo(ProductProduct::class, 'product_id');
+    }
 }
